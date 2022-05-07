26fae726d5677d3430a65f2fdd9858574b0aa017
jpcreativeworks <jp.graphics.011@gmail.com>
2022-05-07 13:25:56 -0700

    connected homepage to middleware paths

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,22 @@
 const express = require("express");
-const parth = require("path");
+const path = require("path");
 const fs = require("fs");
+const { Server } = require("http");
 
 const PORT = 8080;
 
 const app = express();
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static ('public'));
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+console.log('server listening on localhost:8080');
+app.listen(8080);
